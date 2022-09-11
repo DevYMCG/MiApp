e499861eb5b058212f30ef35d4fc90d363deb693
Yohana Contreras <yohana.contrerasg@gmail.com>
2022-09-11 11:52:26 -0400

    Conjunto de scaffolds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_11_030128) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_11_151429) do
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.text "description"
+    t.string "isbn"
+    t.integer "page_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.string "breed"
